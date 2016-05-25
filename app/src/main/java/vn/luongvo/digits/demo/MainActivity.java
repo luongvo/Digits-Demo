c17f89088f34680b4728dffc49df5cff5e7c9cd2
Luong Vo <vodailuong@gmail.com>
2016-05-25 16:53:17 +0700

    [f] Integrate Digits plugin

--- app/src/main/java/vn/luongvo/digits/demo/MainActivity.java
+++ app/src/main/java/vn/luongvo/digits/demo/MainActivity.java
@@ -3,8 +3,14 @@ package vn.luongvo.digits.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.digits.sdk.android.Digits;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
- * @author LuongV
+ * @author Luong Vo
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY,
+                BuildConfig.TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits());
     }
 }
