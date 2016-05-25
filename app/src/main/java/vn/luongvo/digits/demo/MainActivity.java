00bb40a95f0a0df103fb07edb6581ac4e1b95519
Luong Vo <vodailuong@gmail.com>
2016-05-25 17:30:59 +0700

    [f] Update UI and add mechanism for sign out

--- app/src/main/java/vn/luongvo/digits/demo/MainActivity.java
+++ app/src/main/java/vn/luongvo/digits/demo/MainActivity.java
@@ -3,6 +3,8 @@ package vn.luongvo.digits.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.digits.sdk.android.AuthCallback;
@@ -30,23 +32,45 @@ public class MainActivity extends AppCompatActivity {
                 BuildConfig.TWITTER_SECRET);
         Fabric.with(this, new TwitterCore(authConfig), new Digits());
 
-        Digits.getSessionManager().clearActiveSession();
+        updateUI();
+    }
 
-        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
-        digitsButton.setAuthTheme(R.style.CustomDigitsTheme);
-        digitsButton.setCallback(new AuthCallback() {
-            @Override
-            public void success(DigitsSession session, String phoneNumber) {
-                // TODO: associate the session userID with your user model
-                Toast.makeText(getApplicationContext(), "Authentication successful for "
-                        + phoneNumber, Toast.LENGTH_LONG).show();
-            }
+    private void updateUI() {
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.bt_digits_auth);
 
-            @Override
-            public void failure(DigitsException exception) {
-                Log.d("Digits", "Sign in with Digits failure", exception);
-            }
-        });
+        DigitsSession digitsSession = Digits.getSessionManager().getActiveSession();
+        if (digitsSession == null || digitsSession.isLoggedOutUser()) {
+            digitsButton.setVisibility(View.VISIBLE);
+            findViewById(R.id.ln_signed_in).setVisibility(View.GONE);
 
+            digitsButton.setAuthTheme(R.style.CustomDigitsTheme);
+            digitsButton.setCallback(new AuthCallback() {
+                @Override
+                public void success(DigitsSession session, String phoneNumber) {
+                    // TODO: associate the session userID with your user model
+                    Toast.makeText(getApplicationContext(), "Authentication successful for "
+                            + phoneNumber, Toast.LENGTH_LONG).show();
+
+                    updateUI();
+                }
+
+                @Override
+                public void failure(DigitsException exception) {
+                    Log.d("Digits", "Sign in with Digits failure", exception);
+                }
+            });
+        } else {
+            digitsButton.setVisibility(View.GONE);
+            findViewById(R.id.ln_signed_in).setVisibility(View.VISIBLE);
+
+            ((TextView) findViewById(R.id.tv_phone)).setText(digitsSession.getPhoneNumber());
+            findViewById(R.id.bt_sign_out).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Digits.getSessionManager().clearActiveSession();
+                    updateUI();
+                }
+            });
+        }
     }
 }
