22a1c69d49e58c26dfb38963bb74e418b0d24a88
Luong Vo <vodailuong@gmail.com>
2016-05-25 17:14:52 +0700

    [f] Make signup example flow

--- app/src/main/java/vn/luongvo/digits/demo/MainActivity.java
+++ app/src/main/java/vn/luongvo/digits/demo/MainActivity.java
@@ -2,8 +2,14 @@ package vn.luongvo.digits.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.digits.sdk.android.AuthCallback;
 import com.digits.sdk.android.Digits;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 
@@ -22,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY,
                 BuildConfig.TWITTER_SECRET);
         Fabric.with(this, new TwitterCore(authConfig), new Digits());
+
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setCallback(new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        });
+
     }
 }
